8a7a2a9859bf6e437c7f5f6523ce9687099b0fbf
shahadat742380 <shahadat.peacockindia@gmail.com>
2024-04-27 16:58:02 +0600

    small change in banner

--- src/components/component/banner.tsx
+++ src/components/component/banner.tsx
@@ -4,13 +4,13 @@ import { Button } from "@/components/ui/button"
 
 export function Banner() {
   return (
-    <div className="bg-[#F8F8F8] w-full py-16">
+    <div className="bg-[#F8F8F8] dark:bg-gray-950 w-full py-16">
       <div className="max-w-7xl mx-auto px-6">
         <div className="text-center max-w-[700px] mx-auto">
-          <h2 className="text-4xl font-extrabold text-gray-900 sm:text-5xl">
+          <h2 className="text-4xl font-extrabold text-gray-900 dark:text-white sm:text-5xl">
             Ready to dominate the world of color? Join Now
           </h2>
-          <p className="mt-4 text-xl text-gray-600">
+          <p className="mt-4 text-xl text-gray-600 dark:text-gray-200">
             Enroll now and start your journey to becoming a color expert today! With Edukarma, you're not just learning;
             you're transforming your creative future.
           </p>
